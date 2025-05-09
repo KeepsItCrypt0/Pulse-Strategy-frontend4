e9761c5a52864a924affcf7409f0e2ec07d1abbb
PulseStrategy <PLSstrategy@gmail.com>
2025-05-09 18:40:47 -0500

    Update abi.js

--- src/abi.js
+++ src/abi.js
@@ -746,3 +746,23 @@ export const contractAbi = [
 		"type": "function"
 	}
 ]
+
+export const vplsAbi = [
+  {
+    constant: true,
+    inputs: [{ name: '_owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: 'balance', type: 'uint256' }],
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_spender', type: 'address' },
+      { name: '_value', type: 'uint256' }
+    ],
+    name: 'approve',
+    outputs: [{ name: 'success', type: 'bool' }],
+    type: 'function'
+  }
+];
